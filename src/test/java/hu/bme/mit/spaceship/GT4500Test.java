e20a7bbe88ad85080b97efb57ab19d1070d0fc2a
LiubovRukhlina <liuba.rukhlina@gmail.com>
2022-05-17 11:39:59 +0200

    mockito

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -6,35 +6,45 @@ import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
-public class GT4500Test {
+public class GT4500Test{
 
   private GT4500 ship;
+  private TorpedoStore MockTS1, MockTS2;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    this.MockTS1 = mock(TorpedoStore.class);
+    this.MockTS2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(MockTS1, MockTS2);
+   
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(MockTS1.fire(1)).thenReturn(true);
+    when(MockTS2.fire(1)).thenReturn(false);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+
+    verify(MockTS1, times(1)).fire(1);
+    verify(MockTS2, times(0)).fire(1);
+ 
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(MockTS1.fire(1)).thenReturn(true);
+    when(MockTS2.fire(1)).thenReturn(true);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+    verify(MockTS1, times(1)).fire(1);
+    verify(MockTS2, times(1)).fire(1);
   }
 
 }
